21d0a6396e0b853831a225415714b3fbb5fc3219
RuneRaiders <hyeonseoban47@gmail.com>
2025-04-04 21:47:02 +0900

    Object 클래스

---
+++ src/lang/object/ObjectMain.java
@@ -0,0 +1,14 @@
+package lang.object;
+
+public class ObjectMain {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.parentMethod();
+        child.childMethod();
+
+        // toString() 은 Object 클래스의 메서드
+        String str = child.toString();
+        System.out.println(str);
+    }
+}
