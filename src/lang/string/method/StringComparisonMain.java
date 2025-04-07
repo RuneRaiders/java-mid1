76aa4f76ed7debe9f1595fc1c3ef2b9a0aa17469
RuneRaiders <hyeonseoban47@gmail.com>
2025-04-07 20:55:52 +0900

    String 클래스 - 주요 메서드1

---
+++ src/lang/string/method/StringComparisonMain.java
@@ -0,0 +1,24 @@
+package lang.string.method;
+
+public class StringComparisonMain {
+
+    public static void main(String[] args) {
+        String str1 = "Hello, Java!"; // 대문자 일부 있음
+        String str2 = "hello, java!"; // 대문자 없음, 모두 소문자
+        String str3 = "Hello, World!";
+
+        System.out.println("str1 equals str2: " + str1.equals(str2));
+        System.out.println("str1 equalsIgnoreCase str2: " + str1.equalsIgnoreCase(str2));
+
+        System.out.println("'a' compare to 'b': " + "a".compareTo("b")); // -1
+        System.out.println("'b' compare to 'a': " + "b".compareTo("a")); // 1
+        System.out.println("'c' compare to 'a': " + "c".compareTo("a")); // 2
+
+        System.out.println("str1 compare to str3: " + str1.compareTo(str3));            // -13
+        System.out.println("str1 compare to str2: " + str1.compareToIgnoreCase(str2));  // 0
+
+        System.out.println("str1 starts with 'Hello' : " + str1.startsWith("Hello"));  // true
+        System.out.println("str1 ends with 'Java!' : " + str1.endsWith("Java!"));  // true
+
+    }
+}
