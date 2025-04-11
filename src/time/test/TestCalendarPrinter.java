a1c212d0eac4511fd769202c25480e1297a417c0
RuneRaiders <hyeonseoban47@gmail.com>
2025-04-11 22:05:28 +0900

    문제와 풀이2

---
+++ src/time/test/TestCalendarPrinter.java
@@ -0,0 +1,47 @@
+package time.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class TestCalendarPrinter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("년도를 입력하세요: ");
+        int year = scanner.nextInt();
+
+        System.out.print("월을 입력하세요: ");
+        int month = scanner.nextInt();
+
+        printCalendar(year, month);
+    }
+
+    private static void printCalendar(int year, int month) {
+        // 첫번째 날짜
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+        
+        // 다음달 첫번째 날짜 직전까지 구하기 위해 선언 및 초기화
+        LocalDate firstDayOfNextMonth = firstDayOfMonth.plusMonths(1);
+
+        // 월요일=1(1%7=1) ...  일요일=7(7%7=0)
+        int offsetWeekDays = firstDayOfMonth.getDayOfWeek().getValue() % 7;
+
+        System.out.println("Su Mo Tu We Th Fr Sa ");
+        for (int i = 0; i < offsetWeekDays; i++) {
+            System.out.print("   "); // 3칸
+        }
+
+        LocalDate dayIterator = firstDayOfMonth;
+
+        // 이번달 말까지 while문을 돌린다.
+        while (dayIterator.isBefore(firstDayOfNextMonth)) {
+            System.out.printf("%2d ", dayIterator.getDayOfMonth());
+            if (dayIterator.getDayOfWeek() == DayOfWeek.SATURDAY) {
+                System.out.println();
+            }
+            dayIterator = dayIterator.plusDays(1);
+        }
+    }
+}
