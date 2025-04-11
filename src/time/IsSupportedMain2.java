a4b422d4e86f3ee9cf687193c75f22a70a4ba6e4
RuneRaiders <hyeonseoban47@gmail.com>
2025-04-11 21:20:30 +0900

    날짜와 시간 조회하고 조작하기2

---
+++ src/time/IsSupportedMain2.java
@@ -0,0 +1,18 @@
+package time;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class IsSupportedMain2 {
+
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        boolean isSupported = now.isSupported(ChronoField.SECOND_OF_MINUTE);
+        System.out.println("isSupported = " + isSupported);
+        if(isSupported){
+            int minute = now.get(ChronoField.SECOND_OF_MINUTE); // Unsupported field: SecondOfMinute
+            System.out.println("minute = " + minute);
+        }
+    }
+}
