1f20b2379532e5d91544d65bad4c2e402d34dc46
RuneRaiders <hyeonseoban47@gmail.com>
2025-04-15 21:58:30 +0900

    문제와 풀이1

---
+++ src/nested/test/OuterClass3Main.java
@@ -0,0 +1,11 @@
+package nested.test;
+
+import nested.test.OuterClass2.InnerClass;
+
+public class OuterClass3Main {
+
+    public static void main(String[] args) {
+        OuterClass3 outer = new OuterClass3();
+        outer.myMethod();
+    }
+}
